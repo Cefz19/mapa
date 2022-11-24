a830fa3af58a9d01207b72740ec0c99e89aa9ba0
Cefz19 <molanddise@gmail.com>
2022-11-23 19:04:44 -0600

    v3

--- mapa/mapa_1/lib/src/ui/page/home/home_controller.dart
+++ mapa/mapa_1/lib/src/ui/page/home/home_controller.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/material.dart' show ChangeNotifier;
+import 'dart:async';
+
+import 'package:flutter/material.dart' show ChangeNotifier, Offset;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'maps_style.dart';
@@ -8,6 +10,9 @@ class HomeController extends ChangeNotifier {
 
   Set<Marker> get markers => _markers.values.toSet();
 
+  final _markersController = StreamController<String>.broadcast();
+  Stream<String> get onMarkerTap => _markersController.stream;
+
   final initialCameraPosition =
       const CameraPosition(target: LatLng(21.116667, -101.683334), zoom: 0.0);
 
@@ -16,12 +21,28 @@ class HomeController extends ChangeNotifier {
   }
 
   void onTap(LatLng position) {
-    final markerId = MarkerId(_markers.length.toString());
+    final id = _markers.length.toString();
+    final markerId = MarkerId(id);
     final marker = Marker(
-      markerId: markerId,
-      position: position,
-    );
+        markerId: markerId,
+        position: position,
+        draggable: true,
+        //anchor: const Offset(0.5, 1),
+        icon: BitmapDescriptor.defaultMarkerWithHue(200),
+        onTap: () {
+          _markersController.sink.add(id);
+        },
+        onDragEnd: (newPosition) {
+          // ignore: avoid_print
+          print('new position $newPosition');
+        });
     _markers[markerId] = marker;
     notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _markersController.close();
+    super.dispose();
   }
 }
