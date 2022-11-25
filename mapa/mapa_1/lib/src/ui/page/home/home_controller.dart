f3fda96bbda6137b136e2ee864ac24842c767882
Cefz19 <molanddise@gmail.com>
2022-11-24 18:24:29 -0600

    v5

--- mapa/mapa_1/lib/src/ui/page/home/home_controller.dart
+++ mapa/mapa_1/lib/src/ui/page/home/home_controller.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart' show ChangeNotifier;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mapa_1/src/helper/assets_to_bite.dart';
 
 import 'maps_style.dart';
 
@@ -13,22 +14,38 @@ class HomeController extends ChangeNotifier {
   final _markersController = StreamController<String>.broadcast();
   Stream<String> get onMarkerTap => _markersController.stream;
 
-  final initialCameraPosition =
-      const CameraPosition(target: LatLng(21.116667, -101.683334), zoom: 0.0);
+  final initialCameraPosition = const CameraPosition(
+    target: LatLng(21.116667, -101.683334),
+    zoom: 15,
+  );
+
+  final _acumuladorIcon = Completer<BitmapDescriptor>();
+
+  HomeController() {
+    assetToBytes('assets/12.png', width: 130).then(
+      (value) {
+        final bitMap = BitmapDescriptor.fromBytes(value);
+        _acumuladorIcon.complete(bitMap);
+      },
+    );
+  }
 
   void onMapCreated(GoogleMapController controller) {
     controller.setMapStyle(mapStyle);
   }
 
-  void onTap(LatLng position) {
+  void onTap(LatLng position) async {
     final id = _markers.length.toString();
     final markerId = MarkerId(id);
+
+    final icon = await _acumuladorIcon.future;
+
     final marker = Marker(
         markerId: markerId,
         position: position,
         draggable: true,
         //anchor: const Offset(0.5, 1),
-        icon: BitmapDescriptor.defaultMarkerWithHue(200),
+        icon: icon,
         onTap: () {
           _markersController.sink.add(id);
         },
