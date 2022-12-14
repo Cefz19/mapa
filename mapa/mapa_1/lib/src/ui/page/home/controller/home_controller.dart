839351e3dfe0ce560fc563189cb551fd5ce2db2b
Cefz19 <molanddise@gmail.com>
2022-12-14 10:03:43 -0600

    botones_zoom v10

--- mapa/mapa_1/lib/src/ui/page/home/controller/home_controller.dart
+++ mapa/mapa_1/lib/src/ui/page/home/controller/home_controller.dart
@@ -107,6 +107,31 @@ class HomeController extends ChangeNotifier {
 
   Future<void> turnOnGPS() => _geolocator.openLocationSettings();
 
+  Future<void> zoomIn() => _zoom(true);
+
+  Future<void> zoomOut() => _zoom(false);
+
+  Future<void> _zoom(bool zoomIn) async {
+    if (_mapController != null) {
+      double zoom = await _mapController!.getZoomLevel();
+      if (!zoomIn) {
+        if (zoom - 1 <= 0) {
+          return;
+        }
+      }
+
+      zoom = zoomIn ? zoom + 1 : zoom - 1;
+
+      final bounds = await _mapController!.getVisibleRegion();
+      final northeast = bounds.northeast;
+      final southwest = bounds.southwest;
+      final center = LatLng((northeast.latitude + southwest.latitude) / 2,
+          (northeast.longitude + southwest.longitude) / 2);
+      final cameraUpdate = CameraUpdate.newLatLngZoom(center, zoom);
+      await _mapController!.animateCamera(cameraUpdate);
+    }
+  }
+
   @override
   void dispose() {
     _positionSubscription?.cancel();
--- mapa/mapa_1/lib/src/ui/page/home/widgets/where_are_you_going.dart
+++ mapa/mapa_1/lib/src/ui/page/home/widgets/where_are_you_going.dart
@@ -16,46 +16,71 @@ class WhereAreYouGoingButton extends StatelessWidget {
       left: 20,
       right: 20,
       child: SafeArea(
-        child: CupertinoButton(
-          onPressed: () async {
-            final route = MaterialPageRoute<SearchResponse>(
-              builder: (_) => const SearchPlacePage(),
-            );
-            final response = await Navigator.push<SearchResponse>(
-              context,
-              route,
-            );
-            if (response != null) {
-              // ignore: use_build_context_synchronously
-              final controller = context.read<HomeController>();
-              controller.setOriginAndDestination(
-                response.origin,
-                response.destination,
-              );
-            }
-          },
-          padding: EdgeInsets.zero,
-          child: Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            CupertinoButton(
               color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 15.0,
-                  offset: Offset(0, 4),
-                ),
-              ],
-            ),
-            child: const Text(
-              'A donde quieres ir',
-              style: TextStyle(
-                color: Colors.black54,
+              padding: const EdgeInsets.all(10),
+              onPressed: context.read<HomeController>().zoomIn,
+              child: const Icon(
+                Icons.add,
+                color: Colors.black87,
               ),
             ),
-          ),
+            const SizedBox(height: 5),
+            CupertinoButton(
+              color: Colors.white,
+              padding: const EdgeInsets.all(10),
+              onPressed: context.read<HomeController>().zoomOut,
+              child: const Icon(
+                Icons.remove,
+                color: Colors.black87,
+              ),
+            ),
+            const SizedBox(height: 20),
+            CupertinoButton(
+              onPressed: () async {
+                final route = MaterialPageRoute<SearchResponse>(
+                  builder: (_) => const SearchPlacePage(),
+                );
+                final response = await Navigator.push<SearchResponse>(
+                  context,
+                  route,
+                );
+                if (response != null) {
+                  // ignore: use_build_context_synchronously
+                  final controller = context.read<HomeController>();
+                  controller.setOriginAndDestination(
+                    response.origin,
+                    response.destination,
+                  );
+                }
+              },
+              padding: EdgeInsets.zero,
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 15.0,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: const Text(
+                  'A donde quieres ir',
+                  style: TextStyle(
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
